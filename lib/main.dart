3138d415747d2446e63bce521e758d9b53a82b5a
Keerthan22-sys <keerthangowdas222@gmail.com>
2022-11-23 00:02:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/keer.jpg'),
+                ),
+                Text(
+                  'Keerthan Gowda S',
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'),
+                ),
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.blue[50],
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                      fontFamily: 'SourceSansPro'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                        ),
+                        title: Text('+91 9845871477',
+                            style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'SourceSansPro',
+                              fontSize: 20.0,
+                            )),
+                      )),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.email,
+                          color: Colors.teal.shade900,
+                        ),
+                        title: Text(
+                          'keerthangowdas222@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
